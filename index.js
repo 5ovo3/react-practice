a824f8c08b7adc57b44c46fce4e46118410171bc
Jonghyup Lee <jonghyuplee@Jonghyupui-MacBookPro.local>
2022-03-06 15:11:01 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
-const port = 4000
+const port = 4000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config/key')
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://5ovo3:5ovo3@cluster0.orols.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
+const { User } = require('./models/User');
+
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world dd');
+  res.send('hello world !! nodemon');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/register', (req, res) => {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
